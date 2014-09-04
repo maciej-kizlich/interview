0a82e123f2d7382fa66f934e16c8ec248e13cd07
Maciej_Kizlich <Maciej_Kizlich@EPPLKRAW0048.budapest.epam.com>
2014-09-04 15:00:20 +0200

    angularjs test - login with http.post

---
+++ src/main/webapp/angular/js/main.js
@@ -0,0 +1,70 @@
+var app = angular.module("app", []);
+
+app.controller("Login", function($scope, $http, transformRequestAsFormPost) {
+
+	$scope.cfdump = "";
+
+	var request = $http({
+		method : "post",
+		url : "http://localhost:8080/interview/j_spring_security_check",
+		transformRequest : transformRequestAsFormPost,
+		data : {
+			username : "library@epam.com",
+			password : "123456",
+			redirect : ""
+		}
+	});
+
+	request.success(function(html) {
+		$scope.cfdump = html;
+
+	});
+}
+
+);
+
+app.factory(
+				"transformRequestAsFormPost",
+				function() {
+
+					function transformRequest(data, getHeaders) {
+
+						var headers = getHeaders();
+						headers["Content-type"] = "application/x-www-form-urlencoded; charset=utf-8";
+						return (serializeData(data));
+
+					}
+
+					return (transformRequest);
+					function serializeData(data) {
+
+						if (!angular.isObject(data)) {
+							return ((data == null) ? "" : data.toString());
+						}
+
+						var buffer = [];
+
+						for ( var name in data) {
+							if (!data.hasOwnProperty(name)) {
+								continue;
+							}
+
+							var value = data[name];
+
+							buffer.push(encodeURIComponent(name)
+									+ "="
+									+ encodeURIComponent((value == null) ? ""
+											: value));
+
+						}
+
+						var source = buffer.join("&").replace(/%20/g, "+");
+
+						return (source);
+
+					}
+				});
+
+app.value("$sanitize", function(html) {
+	return (html);
+});
